9014231a082e558e3ab8da4a7ae2a4e0ea379c17
vlev322 <vlev322@gmail.com>
2019-12-27 14:40:51 +0200

    feature(comp/header/header-composition)textWelcome

--- src/ts/view/components/header/header-composition/index.tsx
+++ src/ts/view/components/header/header-composition/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import { Carousel } from "../../slider";
-import { Context } from "../../menu/context";
-import { Menu } from "../../menu";
+import { Context } from "../../navigation/context";
 
 import img_1 from "../../../../../style/img/slider/1.jpg";
 import img_2 from "../../../../../style/img/slider/2.jpg";
@@ -10,6 +9,8 @@ import img_3 from "../../../../../style/img/slider/3.jpg";
 import img_4 from "../../../../../style/img/slider/4.jpg";
 import img_5 from "../../../../../style/img/slider/5.jpg";
 import img_6 from "../../../../../style/img/slider/6.jpg";
+import { Welcome } from "./welcome-composition";
+import { Navigation } from "../../navigation/nav-composition";
 
 const images = [img_1, img_2, img_3, img_4, img_5, img_6];
 
@@ -23,7 +24,8 @@ export function HeaderComposition(): JSX.Element {
 	return (
 		<header>
 			<Carousel images={images} />
-			<Menu context={Context}/>
+			<Welcome />
+			<Navigation context={Context} />
 		</header>
 	);
 }
---
+++ src/ts/view/components/header/header-composition/welcome-composition/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Welcome() {
+	return (
+		<div className="welcome-box">
+			<h1>Правильне харчування</h1>
+		</div>
+	);
+}
