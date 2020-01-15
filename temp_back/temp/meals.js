d0f2e74b4363f4a259f89fc43bb26e89013e0433
vlev322 <vlev322@gmail.com>
2020-01-15 08:44:24 +0200

    chore(temp_back/temp/meals) add price, meals

--- temp_back/temp/meals.js
+++ temp_back/temp/meals.js
@@ -6,14 +6,16 @@ const meals = {
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 66,
 			days: [0, 1, 2, 3, 4, 5]
 		},
 		{
 			id: 2,
-			name: "Голубці",
+			name: "Капуста",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 18,
 			days: [0, 3, 4]
 		},
 		{
@@ -22,7 +24,8 @@ const meals = {
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [1, 2]
+			price: 24,
+			days: [0, 1, 2, 5, 6]
 		},
 		{
 			id: 4,
@@ -30,7 +33,8 @@ const meals = {
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [0, 1, 4]
+			price: 83,
+			days: [0, 1, 4, 5, 6]
 		},
 		{
 			id: 5,
@@ -38,7 +42,8 @@ const meals = {
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [3, 4]
+			price: 61,
+			days: [0, 3, 4, 6]
 		},
 		{
 			id: 6,
@@ -46,85 +51,104 @@ const meals = {
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [0, 1, 2, 3]
+			price: 75,
+			days: [0, 1, 2, 3, 6]
 		}
 	],
 	greek: [
 		{
-			id: 1,
+			id: 7,
 			name: "Грецький салат",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [0, 1, 2, 3, 4]
+			price: 64,
+			days: [0, 1, 2, 3, 4, 6]
 		},
 		{
-			id: 2,
+			id: 8,
 			name: "Дакос",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [1, 3, 4]
+			price: 42,
+			days: [0, 1, 3, 4, 6]
 		},
 		{
-			id: 3,
+			id: 9,
 			name: "Фава",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [0, 1, 2, 3]
+			price: 25,
+			days: [0, 1, 2, 3, 6]
 		}
 	],
 	italian: [
 		{
-			id: 1,
+			id: 10,
 			name: "Паста",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [0, 1, 2, 3, 4]
+			price: 85,
+			days: [0, 1, 2, 3, 4, 6]
 		},
 		{
-			id: 2,
+			id: 11,
 			name: "Равіолі",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [1, 3, 4]
+			price: 63,
+			days: [0, 1, 3, 4, 6]
 		},
 		{
-			id: 3,
+			id: 12,
 			name: "Рікота",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 47,
 			days: [0, 1, 2, 3]
 		}
 	],
 	chinese: [
 		{
-			id: 1,
+			id: 13,
 			name: "Тофу",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 36,
 			days: [0, 1, 2, 3, 4]
 		},
 		{
-			id: 2,
+			id: 15,
 			name: "Рис",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
-			days: [1, 3, 4]
+			price: 15,
+			days: [0, 1, 3, 4, 6]
 		},
 		{
-			id: 3,
+			id: 16,
 			name: "Локшина",
 			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
 			callories: 541,
 			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 51,
 			days: [0, 1, 2, 3]
+		},
+		{
+			id: 17,
+			name: "Курка",
+			description: "Reprehenderit nulla exercitation deserunt dolor consectetur culpa deserunt anim minim.",
+			callories: 541,
+			image: "Qui adipisicing minim adipisicing enim culpa mollit esse reprehenderit.",
+			price: 89,
+			days: [0, 1, 2, 3, 4, 5, 6]
 		}
 	]
 };
