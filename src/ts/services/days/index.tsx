6e0fe2bc346f857e9f12335a8821c5c609d46c30
vlev322 <vlev322@gmail.com>
2020-01-27 22:47:19 +0200

    refactor(ts/services/days)refactor function getDays()

--- src/ts/services/days/index.tsx
+++ src/ts/services/days/index.tsx
@@ -1,20 +1,21 @@
-enum DAYS {
-	SUN = "sunday",
-	MON = "monday",
-	TUE = "tuesday",
-	WED = "wednesday",
-	THU = "thursday",
-	FRI = "friday",
-	SAT = "saturday"
-}
-
-export const getDays = () => {
-	let days: string[] = [];
-	for (const day in DAYS) {
-		// days.push(DAYS[day])
-	}
-	return ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-	return days;
+type days = {
+	[index: string]: string;
 };
 
-// let days = getDays();
+const DAYS: days = {
+	SUN: "sunday",
+	MON: "monday",
+	TUE: "tuesday",
+	WED: "wednesday",
+	THU: "thursday",
+	FRI: "friday",
+	SAT: "saturday"
+};
+
+export const getDays = () => {
+	let days = [];
+	for (const day in DAYS) {
+		days.push(DAYS[day]);
+	}
+	return days;
+};
